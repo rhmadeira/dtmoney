4578797cff11c1027621b613d93bd22a4a0f0fea
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2023-01-15 08:40:02 -0300

    feat: Component header under construction

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+import { Container, Content } from "./styles";
+
+export default function Transactions() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
